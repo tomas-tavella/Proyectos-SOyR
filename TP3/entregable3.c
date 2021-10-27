47cf1cfb4df9e507fdb90e040991bee449b1888a
Tomas Tavella <tomas.tavella28@gmail.com>
2021-10-27 11:38:25 -0300

    Agregar bibliotecas de semaforos e IPC

--- TP3/entregable3.c
+++ TP3/entregable3.c
@@ -1,7 +1,10 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include <sys/shm.h>
+#include <sys.sem.h>
+#include <sys/ipc.h>
 
 int main(int argc, char *argv[]){
     // Codigo
