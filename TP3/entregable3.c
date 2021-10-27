5b1fa96e9ff17acc9f83841816a8530882ac9752
Tomas Tavella <tomas.tavella28@gmail.com>
2021-10-27 11:43:44 -0300

    Agregar la union para los semaforos

--- TP3/entregable3.c
+++ TP3/entregable3.c
@@ -6,6 +6,16 @@
 #include <sys.sem.h>
 #include <sys/ipc.h>
 
+// Crear la union necesaria para semctl (Linux-specific)
+// Union es como struct, pero solo uno de los elementos puede tomar una valor a la vez
+union semun {
+    int val;
+    struct semid_ds *buf;
+    unsigned short *array;
+    struct seminfo *__buf;
+};
+
+
 int main(int argc, char *argv[]){
     // Codigo
     return 0;
