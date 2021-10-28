bb90d9cd78cf13dfadd311774dc2f1b5e5b87368
Tomas Tavella <tomas.tavella28@gmail.com>
2021-10-28 13:31:44 -0300

    Agregar la obtencion de memoria compartida con shmget()

--- TP3/entregable3.c
+++ TP3/entregable3.c
@@ -11,6 +11,9 @@
 #define NUMERO 30               // Grupo 30 tuki :basa:
 #define ARCHIVO "/dev/null"
 
+// Cantidad para el tamano de la memoria compartida
+#define CANTIDAD 100
+
 // Definir las operaciones para los semaforos
 #define BLOQUEAR(OP) ((OP).sem_op = -1)             // OP es una estructura
 #define DESBLOQUEAR(OP) ((OP).sem_op = +1)
@@ -24,18 +27,29 @@ union semun {
     struct seminfo *__buf;
 };
 
+struct datos{
+    unsigned short id;
+    float tiempo; //Este no se que ponerle
+    char dato[30];
+}
+
 
 int main(int argc, char *argv[]){
-    // Codigo
     key_t clave;
-    int id_memoria;
+    int IDmem;
 
+    // Obtener la clave, verificando si la pudo conseguir
     clave = ftok(ARCHIVO,NUMERO);
-    if (Clave == (key_t)-1){
+    if (Clave == (key_t) -1){
 		printf("No consegui  clave para memoria compartida\n");
 		exit(1);
 	}
 
-    
+    // Llamar al sistema para obtener la memoria compartida
+    IDmem = shmget(clave, CANTIDAD*sizeof(datos), 0666 | IPC_CREAT);
+    if(IDmem == -1){
+		printf("No consegui ID para memoria compartida\n");
+		exit (2);
+	}
     return 0;
 }
