d25fc7f4861f9726fbaa0803d1f27f6c2aac0c67
Tomas Tavella <tomas.tavella28@gmail.com>
2021-10-28 18:41:28 -0300

    Eliminar las partes de semaforos de los archivos para colas

--- TP3/Colas/consumidor_col.c
+++ TP3/Colas/consumidor_col.c
@@ -4,7 +4,6 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/shm.h>
-#include <sys/sem.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <sys/time.h>
@@ -19,24 +18,6 @@ FILE *fpcsv;
 // Cantidad para el tamano de la memoria compartida
 #define CANTIDAD 100
 
-// Defino los distintos semaforos
-#define SEM_SYNC 0
-#define SEM_READ 1
-#define SEM_WRITE 2
-
-// Definir las operaciones para los semaforos
-#define BLOQUEAR(OP) ((OP).sem_op = -1)             // OP es una estructura
-#define DESBLOQUEAR(OP) ((OP).sem_op = +1)
-
-// Crear la union necesaria para semctl (Linux-specific)
-// Union es como struct, pero solo uno de los elementos puede tomar una valor a la vez
-union semun {
-    int val;
-    struct semid_ds *buf;
-    unsigned short *array;
-    struct seminfo *__buf;
-};
-
 // Estructura de datos a escribir en el buffer
 struct datos{
     int id;
@@ -46,10 +27,8 @@ struct datos{
 
 int main(int argc, char *argv[]){
     key_t clave;
-    int IDmem, IDsem;
+    int IDmem;
     struct datos *memoria_comp = NULL;
-    union semun argumento;
-    struct sembuf op;
 
     // Obtener la clave, verificando si la pudo conseguir
     clave = ftok(PATH,NUMERO);
@@ -71,16 +50,6 @@ int main(int argc, char *argv[]){
 		printf("No se pudo asociar el puntero a la memoria compartida\n");
 		exit(3);
 	}
-
-    // Creación de semaforos
-    IDsem = semget(clave, 3, 0666 | IPC_CREAT);
-    if (IDsem == -1){
-        printf("No se puede crear el semáforo\n");
-        exit(4);
-    }
-
-    // Inicialización de semaforos
-    op.sem_flg = 0;                 // Nunca usamos flags para los semaforos
 
     // Verificar que el archivo exista
     fpcsv = fopen("datos.csv","w");
--- TP3/Colas/productor_col.c
+++ TP3/Colas/productor_col.c
@@ -4,7 +4,6 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/shm.h>
-#include <sys/sem.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <sys/time.h>                   // Para obtener el tiempo de UNIX
@@ -19,24 +18,6 @@ FILE *fpdat;
 // Cantidad para el tamano de la memoria compartida
 #define CANTIDAD 100
 
-// Defino los distintos semaforos
-#define SEM_SYNC 0
-#define SEM_READ 1
-#define SEM_WRITE 2
-
-// Definir las operaciones para los semaforos
-#define BLOQUEAR(OP) ((OP).sem_op = -1)             // OP es una estructura
-#define DESBLOQUEAR(OP) ((OP).sem_op = +1)
-
-// Crear la union necesaria para semctl (Linux-specific)
-// Union es como struct, pero solo uno de los elementos puede tomar una valor a la vez
-union semun {
-    int val;
-    struct semid_ds *buf;
-    unsigned short *array;
-    struct seminfo *__buf;
-};
-
 // Estructura de datos a escribir en el buffer
 struct datos{
     int id;
@@ -47,11 +28,9 @@ struct datos{
 
 int main(int argc, char *argv[]){
     key_t clave;
-    int IDmem, IDsem;
+    int IDmem;
     struct datos *memoria_comp = NULL;
-    union semun argumento;
     struct timeval tiempo;
-    struct sembuf op;
     suseconds_t tiempo_init;
 
     // Obtener la clave, verificando si la pudo conseguir
@@ -74,23 +53,6 @@ int main(int argc, char *argv[]){
 		printf("No se pudo asociar el puntero a la memoria compartida\n");
 		exit(3);
 	}
-
-    // Creación de semaforos
-    IDsem = semget(clave, 3, 0666 | IPC_CREAT);
-    if (IDsem == -1){
-        printf("No se puede crear el semáforo\n");
-        exit(4);
-    }
-
-    // Inicialización de semaforos
-    op.sem_flg = 0;                 // Nunca usamos flags para los semaforos
-
-    argumento.val = 1; //Semaforo de sincronizacion inicializado en verde
-    semctl (IDsem, SEM_SYNC, SETVAL, argumento);
-    argumento.val = 0; //Semaforo de lectura inicializado en rojo
-    semctl (IDsem, SEM_READ, SETVAL, argumento);
-    argumento.val = 1; //Semaforo de escritura inicializado en verde
-    semctl (IDsem, SEM_WRITE, SETVAL, argumento);
 
     // Verificar que el archivo exista
     fpdat = fopen("datos.dat","rb");
@@ -116,20 +78,6 @@ int main(int argc, char *argv[]){
         fread(&(buffer.dato),sizeof(struct datos),1,fpdat);
         memcomp_cnt++;
         id++;
-
-        if (memcomp_cnt == CANTIDAD/2){
-            op.sem_num = SEM_WRITE;
-            BLOQUEAR(op);
-            semop(IDsem, &op, 3);
-
-        }
-        if (memcomp_cnt == CANTIDAD){
-            memcomp_cnt=0;
-            
-            op.sem_num = SEM_READ;
-            BLOQUEAR(op);
-            semop(IDsem, &op, 3);
-        }
     }
     fclose(fpdat);
 
