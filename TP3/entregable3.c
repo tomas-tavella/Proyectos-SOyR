dfb389338a0da8f925e92ba5fd7cc54c3f395e42
Tomas Tavella <tomas.tavella28@gmail.com>
2021-10-28 13:54:35 -0300

    Agregar la llamada a semaforos y el puntero a archivo

--- TP3/entregable3.c
+++ TP3/entregable3.c
@@ -7,8 +7,11 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
+// Definir puntero al archivo a utilizar
+FILE *fpdat;
+
 // Definir los parametros de ftok
-#define NUMERO 30               // Grupo 30 tuki :basa:
+#define NUMERO 30      
 #define PATH "/dev/null"
 
 // Cantidad para el tamano de la memoria compartida
@@ -29,14 +32,14 @@ union semun {
 
 struct datos{
     unsigned short id;
-    float tiempo; //Este no se que ponerle
+    float tiempo;               //Este no se que ponerle
     char dato[30];
 }buffer;
 
 
 int main(int argc, char *argv[]){
     key_t clave;
-    int IDmem;
+    int IDmem, IDSem;
     struct datos *memoria_comp = NULL;
 
     // Obtener la clave, verificando si la pudo conseguir
@@ -50,14 +53,23 @@ int main(int argc, char *argv[]){
     IDmem = shmget(clave, CANTIDAD*sizeof(buffer), 0666 | IPC_CREAT);
     if(IDmem == -1){
 		printf("No se pudo obtener un ID de memoria compartida\n");
-		exit (2);
+		exit(2);
 	}
+
+    // Llamar al sistema para obtener los semaforos
+    IDSem = semget(clave, 3, 0666 | IPC_CREAT);
+    if (IDSem == -1){
+        printf("No se puede crear el semáforo\n");
+        exit(0);
+    }
 
     // Adosar el proceso al espacio de memoria mediante un puntero
     memoria_comp = (struct datos *) shmget(clave, NULL, NULL)
     if (memoria_comp == NULL){
 		printf("No se pudo asociar el puntero a la memoria compartida\n");
-		exit (3);
+		exit(3);
 	}
+
+    fclose(fpdat);
     return 0;
 }
