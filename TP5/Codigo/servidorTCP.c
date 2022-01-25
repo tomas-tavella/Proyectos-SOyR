af665344be028975a60bd13e8b11fcfb169ae8f0
Grobook <juanpablo.elisei@gmail.com>
2022-01-25 12:38:00 -0300

    Modificado ejemplo para cumplir con los requerimientos iniciales

--- TP5/Codigo/servidorTCP.c
+++ TP5/Codigo/servidorTCP.c
@@ -24,77 +24,72 @@ void handler(int sig);
 //===== Main program ========================================================
 int main(int argc, char *argv[]) {
 
-  unsigned int         server_s;        // Descriptor del socket
-  unsigned int         connect_s;       // Connection socket descriptor
-  struct sockaddr_in   server_addr;     // Estructura con los datos del servidor
-  struct sockaddr_in   client_addr;     // Estructura con los datos del cliente
-  struct in_addr       client_ip_addr;  // Client IP address
-  int                  addr_len;        // Tamaño de las estructuras
-  char                 buf_tx[1500];    // Buffer de 1500 bytes para los datos a transmitir
-  char                 buf_rx[1500];    // Buffer de 1500 bytes para los datos a transmitir
-  int                  bytesrecibidos, bytesaenviar, bytestx;  // Contadores
-  int                  i=0;             //contador de mensajes
-  pid_t pid_n;
+    unsigned int         server_s;        // Descriptor del socket
+    unsigned int         connect_s;       // Connection socket descriptor
+    struct sockaddr_in   server_addr;     // Estructura con los datos del servidor
+    struct sockaddr_in   client_addr;     // Estructura con los datos del cliente
+    struct in_addr       client_ip_addr;  // Client IP address
+    int                  addr_len;        // Tamaño de las estructuras
+    char                 buf_tx[1500];    // Buffer de 1500 bytes para los datos a transmitir
+    char                 buf_rx[1500];    // Buffer de 1500 bytes para los datos a transmitir
+    int                  bytesrecibidos, bytesaenviar, bytestx;  // Contadores
+    int                  i=0;             //contador de mensajes
+    pid_t pid_n;
 
-  signal(SIGHUP,handler);
-  server_s = socket(AF_INET, SOCK_STREAM, SOCKET_PROTOCOL);
-  if (server_s==-1) {
-    perror("socket");
-    return 1;
-  }
-  printf("Cree el descriptor del socket %d\n",server_s);
-  
-  server_addr.sin_family      = AF_INET;            // Familia TCP/IP
-  server_addr.sin_port        = htons(PORT_NUM);    // Número de Port, htons() lo convierte al orden de la red
-  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);  // INADDR_ANY = cualquier direccion IP, htonl() lo convierte al orden de la red
-  
-  bind(server_s, (struct sockaddr *)&server_addr, sizeof(server_addr));
-  
-  printf("asocie el descriptor %u con el port %u acepta conexiones desde %u\n", server_s,PORT_NUM, INADDR_ANY);
-  
-  listen(server_s, NCONCUR);
-
-  addr_len = sizeof(client_addr);
-  while(!terminar) {   
-    connect_s = accept(server_s, (struct sockaddr *)&client_addr, &addr_len);
-    if (connect_s==-1) {
-      perror("accept");
-      return 2;
+    signal(SIGHUP,handler);
+    server_s = socket(AF_INET, SOCK_STREAM, SOCKET_PROTOCOL);
+    if (server_s==-1) {
+        perror("socket");
+        return 1;
     }
-    printf("Soy el padre  %d y digo  que el IP del cliente es: %s y el port del cliente es %hu \n",getpid(),    
-                inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-    if((pid_n=fork())==0) {
-      do {
-        close (server_s); 
-        sprintf(buf_tx,"Mensaje de prueba %d",i++); // creo un mensaje numrado para enviar
-        bytesaenviar =  strlen(buf_tx);
-        bytestx=send(connect_s, buf_tx, bytesaenviar, 0);
-        if (bytestx==-1)
-          {
-          perror ("send");
-          return 3;
-          }
-        printf("Envie el mensaje %s con %d bytes\n", buf_tx, bytestx);
-        buf_tx[0]='\n'; //(un solo byte)
-        bytestx=send(connect_s, buf_tx, 1 , 0);
-        bytesrecibidos=recv(connect_s, buf_rx, sizeof(buf_rx), 0);
-        if (bytesrecibidos==-1) {
-          perror ("recv");
-          return 3;
+    printf("Creado el descriptor del socket %d\n",server_s);
+  
+    server_addr.sin_family      = AF_INET;            // Familia TCP/IP
+    server_addr.sin_port        = htons(PORT_NUM);    // Número de Port, htons() lo convierte al orden de la red
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);  // INADDR_ANY = cualquier direccion IP, htonl() lo convierte al orden de la red
+  
+    bind(server_s, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    printf("Asociado el descriptor %u con el port %u\n", server_s,PORT_NUM);
+  
+    listen(server_s, NCONCUR);
+
+    addr_len = sizeof(client_addr);
+    while(!terminar) {   
+        connect_s = accept(server_s, (struct sockaddr *)&client_addr, &addr_len);
+        if (connect_s==-1) {
+            perror("accept");
+            return 2;
         }
-        printf("Recibi %d bytes del cliente con texto = '%s' \n", bytesrecibidos, buf_rx);
-        } while (strncmp(buf_rx,"FIN",3)!=0); 
-      close(connect_s);
-      return 0; 
-      }
-    else {
-      printf("Soy el padre %d, recibi un pedido de conexión, la derive a mi hijo %d\n", getpid(),pid_n); 
-      close(connect_s);
-      } 
-  }  
-  wait(NULL);
-  close(server_s);
-  return 0;
+        printf("Nueva conexión desde: %s:%hu\n",inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        if((pid_n=fork())==0) {
+            do {
+                printf("Proceso hijo %d atendiendo conexión desde: %s\n",getpid(),inet_ntoa(client_addr.sin_addr));
+                close (server_s); // Cerrar socket no usado
+
+                sprintf(buf_tx,"Listo"); // Mensaje de listo para recibir
+                bytesaenviar =  strlen(buf_tx);
+                bytestx=send(connect_s, buf_tx, bytesaenviar, 0);
+                if (bytestx==-1) {
+                    perror ("send");
+                    return 3;
+                }
+
+                bytesrecibidos=recv(connect_s, buf_rx, sizeof(buf_rx), 0);
+                if (bytesrecibidos==-1) {
+                    perror ("recv");
+                    return 3;
+                }
+                // Verificar que se recibe "archivo" primero, armar string con el nombre, y ver como manejar la recepcion del archivo
+            } while (strncmp(buf_rx,"FIN",3)!=0); 
+            close(connect_s);
+            return 0; 
+        } else {
+            close(connect_s);
+        } 
+    }
+    wait(NULL);
+    close(server_s);
+    return 0;
 }
 
 
